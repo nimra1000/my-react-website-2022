4ad64faa1fbf02a3f9a9597cf0a425d2e2b3df67
nimra ajmal <nimra_123@hotmail.co.uk>
2022-04-10 18:10:51 +0100

    added navbar via react bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
-const App = () => {
- return <h1>This is my React app!</h1>;
-}
-ReactDOM.render(<App />, document.getElementById(‘app'));
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

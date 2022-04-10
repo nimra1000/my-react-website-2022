0b9b8ac73a500b9059e8cb583250753f0923e9c2
nimra ajmal <nimra_123@hotmail.co.uk>
2022-04-10 17:07:40 +0100

    setup

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 const App = () => {
  return <h1>This is my React app!</h1>;
- }
+}
 ReactDOM.render(<App />, document.getElementById(‘app'));
